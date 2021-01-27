d333e41d9af6649ad753d7e2b08e6867efd610d8
Сабина Кадырова <sabina@Sabinas-MacBook-Pro.local>
2021-01-27 15:09:03 +0200

    add butten Toggle random charter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
-import Header from '../header';
-import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
-
+import React, { useState } from "react";
+import { Col, Row, Container } from "reactstrap";
+import Header from "../header";
+import RandomChar from "../randomChar";
+import ItemList from "../itemList";
+import CharDetails from "../charDetails";
+import "./app.css";
 
 const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+  const [toggler, setToggle] = useState(true);
+  return (
+    <>
+      <Container>
+        <Header />
+      </Container>
+      <Container>
+        <Row>
+          <Col lg={{ size: 5, offset: 0 }}>
+            {toggler ? <RandomChar /> : null}
+            <button onClick={() => setToggle(!toggler)}>
+              Toggle random charter
+            </button>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="6">
+            <ItemList />
+          </Col>
+          <Col md="6">
+            <CharDetails />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default App;
